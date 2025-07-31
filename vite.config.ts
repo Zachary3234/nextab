64d9d4ed79959db53d4a42f6ccb2ec3454f252c9
Zachary3234 <760495604@qq.com>
2025-07-31 13:24:36 +0000

    change base for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://cn.vitejs.dev/guide/
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/nextab/',
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   server: {
     allowedHosts: ['.nexcab.top'],
